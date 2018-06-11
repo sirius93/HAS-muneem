7500b7ba9db913ca16db64de5a905bfcba16454a
Nandan Kumar <nkumar101@sapient.com>
2018-06-11 22:16:02 +0530

    updated js to modular

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
-function initAccordion(accordionElem){
-  
-    //when panel is clicked, handlePanelClick is called.          
-    function handlePanelClick(event){
-        showPanel(event.currentTarget);
+var hasMuneem = (function () {
+    function initFirebase(config){
+        firebase.initializeApp(config); 
     }
-  //Hide currentPanel and show new panel.  
+
+    function initAccordion(accordionElem){
+        //when panel is clicked, handlePanelClick is called.          
+        function handlePanelClick(event){
+            showPanel(event.currentTarget);
+        }
+        //Hide currentPanel and show new panel.  
+        
+        function showPanel(panel){
+            //Hide current one. First time it will be null. 
+            var expandedPanel = accordionElem.querySelector(".active");
+            if (expandedPanel){
+                expandedPanel.classList.remove("active");
+            }
+            //Show new one
+            panel.classList.add("active");
+        }
+        var allPanelElems = accordionElem.querySelectorAll(".panel");
+        for (var i = 0, len = allPanelElems.length; i < len; i++){
+                allPanelElems[i].addEventListener("click", handlePanelClick);
+        }
+        //By Default Show first panel
+        showPanel(allPanelElems[0])
+    }
     
-    function showPanel(panel){
-      //Hide current one. First time it will be null. 
-       var expandedPanel = accordionElem.querySelector(".active");
-       if (expandedPanel){
-           expandedPanel.classList.remove("active");
-       }
-       //Show new one
-       panel.classList.add("active");
-    }
-    var allPanelElems = accordionElem.querySelectorAll(".panel");
-    for (var i = 0, len = allPanelElems.length; i < len; i++){
-         allPanelElems[i].addEventListener("click", handlePanelClick);
-    }
-    //By Default Show first panel
-    showPanel(allPanelElems[0])
-  }
-  initAccordion(document.getElementById("accordion"));
+    return {
+        initFirebase:initFirebase,
+        initAccordion:initAccordion
+    }; 
+})(); 
+hasMuneem.initFirebase(config);
+hasMuneem.initAccordion(document.getElementById("accordion"));
