2a537db36620fcb409f2c85492f1373e39f518ad
Nandan Kumar <nkumar101@sapient.com>
2018-06-11 19:16:32 +0530

    initial commit with accordion

---
+++ script.js
@@ -0,0 +1,25 @@
+function initAccordion(accordionElem){
+  
+    //when panel is clicked, handlePanelClick is called.          
+    function handlePanelClick(event){
+        showPanel(event.currentTarget);
+    }
+  //Hide currentPanel and show new panel.  
+    
+    function showPanel(panel){
+      //Hide current one. First time it will be null. 
+       var expandedPanel = accordionElem.querySelector(".active");
+       if (expandedPanel){
+           expandedPanel.classList.remove("active");
+       }
+       //Show new one
+       panel.classList.add("active");
+    }
+    var allPanelElems = accordionElem.querySelectorAll(".panel");
+    for (var i = 0, len = allPanelElems.length; i < len; i++){
+         allPanelElems[i].addEventListener("click", handlePanelClick);
+    }
+    //By Default Show first panel
+    showPanel(allPanelElems[0])
+  }
+  initAccordion(document.getElementById("accordion"));
