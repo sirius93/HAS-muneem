099cba96ee68c6090bd7bb4c53bc25b9a82645d5
Nandan Kumar <nkumar101@sapient.com>
2018-06-15 19:58:25 +0530

    added login page for extra auth layer

--- script.js
+++ script.js
@@ -7,45 +7,80 @@ var hasMuneem = (function () {
             var users = (snapshot.val() && snapshot.val()) || 'Anonymous';
             var template = document.getElementById('accordion-render');
             var accordionList = document.getElementById('accordion');
-            var templateHtml = template.innerHTML;
+            var templateHtml = template?template.innerHTML:'';
             var listHtml = '';
             for(var user in users){
-                listHtml += templateHtml.replace(/{{employee-name}}/g, users[user].Name);
-                console.log(user,users);
+                listHtml += templateHtml.replace(/{{employee-name}}/g, users[user].Name)
+                .replace(/{{employee-id}}/g, user)
+                .replace(/{{moneyCount}}/g, users[user].moneyCount);;
             }
-            accordionList.innerHTML += listHtml;
+            if(accordionList){
+                accordionList.innerHTML += listHtml;
+            }
             hasMuneem.initAccordion(document.getElementById('accordion'));
         });
     }
-
-    function writeData(){
-        database().ref('users/' + Nandan).set({
-            'money-count' :20
-          });
+    function fireBaseLogin(_that){
+        var targetForm = _that.event.target.parentNode;
+        var targerUsername = targetForm.elements[0].value;
+        var targerPassword = targetForm.elements[1].value;
+        auth.signInWithEmailAndPassword(targerUsername, targerPassword)
+        .catch(function(error) {            
+            console.log(error.message);
+        })
+        auth.onAuthStateChanged(user => {
+            if(user) {
+              window.location = 'amount-update.html';
+            }
+        });
+    }
+    function formData(_that){
+        var targetForm = _that.event.target.parentNode;
+        var targerUser = targetForm.name;
+        var oldCount = targetForm.dataset.moneycount;
+        var formDataObject = {};
+        var newCount = parseInt(oldCount) + parseInt(targetForm.elements[0].value);
+        formDataObject['moneyCount'] = newCount;
+        formDataObject['lastDate'] = new Date();
+        hasMuneem.writeData(formDataObject,targerUser);
+    }
+    function writeData(data,user){
+        database.ref('users/' + user)
+        .update(data,
+        function(error){
+            if(error){
+                console.log(error);
+            }else{
+                console.log('data saved successfully');
+                location.reload();
+            }
+        })
     }
     function initAccordion(accordionElem){
-        //when panel is clicked, handlePanelClick is called.          
-        function handlePanelClick(event){
-            showPanel(event.currentTarget);
-        }
-        //Hide currentPanel and show new panel.  
-        
-        var allPanelElems = accordionElem.querySelectorAll(".panel");
-        for (var i = 0, len = allPanelElems.length; i < len; i++){
-                allPanelElems[i].addEventListener("click", handlePanelClick);
-        }
-        //By Default Show first panel
-        showPanel(allPanelElems[0]);
-
-        function showPanel(panel){
-            //Hide current one. First time it will be null. 
-            var expandedPanel = accordionElem.querySelector(".active");
-            if (expandedPanel){
-                expandedPanel.classList.remove("active");
+        if(accordionElem){
+            //when panel is clicked, handlePanelClick is called.          
+            function handlePanelClick(event){
+                showPanel(event.currentTarget);
             }
-            //Show new one
-            if(panel){
-                panel.classList.add("active");
+            //Hide currentPanel and show new panel.  
+            
+            var allPanelElems = accordionElem.querySelectorAll(".panel");
+            for (var i = 0, len = allPanelElems.length; i < len; i++){
+                    allPanelElems[i].addEventListener("click", handlePanelClick);
+            }
+            //By Default Show first panel
+            // showPanel(allPanelElems[0]);
+
+            function showPanel(panel){
+                //Hide current one. First time it will be null. 
+                var expandedPanel = accordionElem.querySelector(".active");
+                if (expandedPanel){
+                    expandedPanel.classList.remove("active");
+                }
+                //Show new one
+                if(panel){
+                    panel.classList.add("active");
+                }
             }
         }
     }
@@ -53,14 +88,23 @@ var hasMuneem = (function () {
     return {
         initFirebase:initFirebase,
         initAccordion:initAccordion,
-        firebaseData:firebaseDatabase
+        firebaseData:firebaseDatabase,
+        formData : formData,
+        writeData: writeData,
+        fireBaseLogin:fireBaseLogin
     }; 
 })(); 
 hasMuneem.initFirebase(config);
 var database = firebase.database();
+var auth = firebase.auth();
 hasMuneem.firebaseData();
 
-function submitData(){
-    this.event.preventDefault();
-    console.log(this)
+function submitData(_this){
+    let _this = this;
+    // _this.event.preventdefault();
+    hasMuneem.formData(_this);
+}
+function loginUser(_this){
+    let _thiscon = this;
+    hasMuneem.fireBaseLogin(_thiscon);
 }
